ef771ab27faef90b2d13da91d14dab431a1f6b05
Nha An <dnhanh95@gmail.com>
2025-04-23 15:32:40 +0700

    Add configuration for GraphQL module and TypeOrm module

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppDataSource } from '../db-config';
+import { join } from 'path';
+
+//check if this is production or development
+const isProduction = process.env.NODE_ENV === 'production';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: !isProduction,
+      introspection: !isProduction,
+    }),
+    TypeOrmModule.forRoot(AppDataSource.options),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
