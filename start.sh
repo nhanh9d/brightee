b75f45362142c32c59b250ae11e8f502e2d5e7ce
Nha An <dnhanh95@gmail.com>
2025-04-25 10:36:56 +0700

    adjust script to run with docker compose, add user guide to readme

--- start.sh
+++ start.sh
@@ -1,23 +1,34 @@
-#run docker compose
-# docker compose up -d
+SHOULD_RUN_DOCKER=false
 
-#run npm install
-echo "Installing dependencies for backend..."
-cd be
-npm install
+for arg in "$@"; do
+  if [ "$arg" = "--docker" ]; then
+    SHOULD_RUN_DOCKER=true
+  fi
+done
 
-#run npm start
-echo "Starting backend..."
-npm run start &
+if [ "$SHOULD_RUN_DOCKER" = true ]; then
+  #run docker compose
+  docker compose up -d
+else
+  #run npm install
+  echo "Installing dependencies for backend..."
+  cd be
+  npm install
 
-#run npm install
-echo "Installing dependencies for frontend..."
-cd ../fe
-npm install
+  #run npm start
+  echo "Starting backend..."
+  npm run start &
 
-#run npm start
-echo "Starting frontend..."
-PORT=3001 npm run start &
+  #run npm install
+  echo "Installing dependencies for frontend..."
+  cd ../fe
+  npm install
 
-#wait for all processes to finish
-wait
+  #run npm start
+  echo "Starting frontend..."
+  PORT=3001 npm run start &
+
+  #wait for all processes to finish
+  wait
+fi
+
